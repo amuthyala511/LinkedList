1cb721c4f3d6de3d28466f589a436196dad37317
Aishwarya <amuthyala511@gmail.com>
2020-11-04 19:32:38 +0530

    Added: Elements at the top of the list

--- LinkedListUsingGenerics/src/main/java/com/blz/linkedlist/generics/MyLinkedList.java
+++ LinkedListUsingGenerics/src/main/java/com/blz/linkedlist/generics/MyLinkedList.java
@@ -1,8 +1,8 @@
 package com.blz.linkedlist.generics;
 
 public class MyLinkedList<K> {
-	public INode head;
-	public INode tail;
+	public static INode head;
+	public static INode tail;
 	
 	public MyLinkedList() {
 		this.head = null;
@@ -10,10 +10,8 @@ public class MyLinkedList<K> {
 	}
 
 	public void add(INode<K> newNode) {
-		if(this.tail == null) {
+		if(this.tail == null && this.head == null) {
 			this.tail = newNode;
-		}
-		if(this.head == null) {
 			this.head = newNode;
 		}
 		else {
--- LinkedListUsingGenerics/src/test/java/com/blz/linkedlist/generics/MyLinkedListTest.java
+++ LinkedListUsingGenerics/src/test/java/com/blz/linkedlist/generics/MyLinkedListTest.java
@@ -4,15 +4,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyLinkedListTest {
+	
 	@Test
-	public void given3Numbers_createLinkedList() {
-		MyNode<Integer> myFirstNode = new MyNode<>(56);
-		MyNode<Integer> mySecondNode = new MyNode<>(30);
-		MyNode<Integer> myThirdNode = new MyNode<>(70);
-		myFirstNode.setNext(mySecondNode);
-		mySecondNode.setNext(myThirdNode);
-		boolean result = myFirstNode.getNext().equals(mySecondNode) && 
-							mySecondNode.getNext().equals(myThirdNode);
+	public void given3Numbers_addAtTheTop() {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+		MyLinkedList<Integer> myList = new MyLinkedList<Integer>();
+		myList.add(myFirstNode);
+		myList.add(mySecondNode);
+		myList.add(myThirdNode);
+		boolean result = MyLinkedList.head.equals(myThirdNode) &&
+					MyLinkedList.head.getNext().equals(mySecondNode) && 
+					MyLinkedList.tail.equals(myThirdNode);
 		Assert.assertEquals(true, result);
 	}
 }
